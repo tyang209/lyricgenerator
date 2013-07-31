735041e983e8cce872e05b1d73e232f0556c1a8f
tyang209 <tyang209@gmail.com>
2013-07-31 16:34:15 -0400

    Added regex

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import urllib2, httplib
-
+import re
 httplib.HTTPConnection.debuglevel = 1
 
 request = urllib2.Request('http://lyrics.wikia.com/Special:Random')
 
+i=0
+while i<10:
 
+	opener = urllib2.build_opener()
 
-opener = urllib2.build_opener()
+	f = opener.open(request)
+	match = re.search(r'LyricFind:(\w+):(\w+)',f.url)
 
-f = opener.open(request)
-print f.url
-#for line in f:
-#	print line
-	
-	
+	artist=match.group(1)
+	song = match.group(2)
+	print f.url
+	print artist, song
+	i=i+1
+
